6d1fdd82c70b4148ef2944d1437cba5bf1675fd5
Ritika Kumari <ritikakumari1343@gmail.com>
2026-01-10 00:39:03 +0530

    add event listeners

--- L7/event handling in JS/script.js
+++ L7/event handling in JS/script.js
@@ -6,22 +6,41 @@ let btn1 = document.querySelector("#btn1");
 
 let box = document.querySelector("div");
 
-box.onmouseover = () => {
-    console.log("Mouse is inside the box!")
-};
+// box.onmouseover = () => {
+//     console.log("Mouse is inside the box!")
+// };
 
 // even object
 
-btn1.onclick = (event) => {
-    console.log(event);
-    console.log(event.type);
-    console.log(event.target);
-    console.log(event.clientX, event.clientY);
+// btn1.onclick = (event) => {
+//     console.log(event);
+//     console.log(event.type);
+//     console.log(event.target);
+//     console.log(event.clientX, event.clientY);
+// };
+
+// box.onclick = (event) => {
+//     console.log(event);
+//     console.log(event.type);
+//     console.log(event.target);
+//     console.log(event.clientX, event.clientY);
+// };
+
+// event listeners: we can add multiple event listeners to a single element
+
+btn1.addEventListener("click", (event) => {
+    console.log("button 1 was clicked!")
+    console.log(event.type); // can access event object here
+});
+
+btn1.addEventListener("click", () => {
+    console.log("button 2 was clicked!")
+});
+
+// removing event listeners
+
+const handlerRemove = () => {
+    console.log("Handler was removed");
 };
 
-box.onclick = (event) => {
-    console.log(event);
-    console.log(event.type);
-    console.log(event.target);
-    console.log(event.clientX, event.clientY);
-};
+btn1.removeEventListener("click",handlerRemove);
