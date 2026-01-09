276ab53e36cf81914a39d4465c98b50555c70bd7
Ritika Kumari <ritikakumari1343@gmail.com>
2026-01-10 00:09:49 +0530

    add event handling in JS

---
+++ L7/event handling in JS/script.js
@@ -0,0 +1,11 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = () => {
+    console.log("Button was Clicked!")
+}
+
+let box = document.querySelector("div");
+
+box.onmouseover = () => {
+    console.log("Mouse is inside the box!")
+};
