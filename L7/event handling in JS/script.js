e40b67aaa28545b8094a267676cbb41e35f15a4d
Ritika Kumari <ritikakumari1343@gmail.com>
2026-01-10 00:20:16 +0530

    add event object

--- L7/event handling in JS/script.js
+++ L7/event handling in JS/script.js
@@ -1,11 +1,27 @@
 let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = () => {
-    console.log("Button was Clicked!")
-}
+// btn1.onclick = () => {
+//     console.log("Button was Clicked!")
+// }
 
 let box = document.querySelector("div");
 
 box.onmouseover = () => {
     console.log("Mouse is inside the box!")
+};
+
+// even object
+
+btn1.onclick = (event) => {
+    console.log(event);
+    console.log(event.type);
+    console.log(event.target);
+    console.log(event.clientX, event.clientY);
+};
+
+box.onclick = (event) => {
+    console.log(event);
+    console.log(event.type);
+    console.log(event.target);
+    console.log(event.clientX, event.clientY);
 };
